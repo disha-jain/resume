2701e706132c5f5809b862e456f4487efd78a74c
Disha <dishajain2016@gmail.com>
2016-12-03 17:48:24 -0500

    Finished mostly all screens
    Mostly all are done

--- CropStop/cropstop.js
+++ CropStop/cropstop.js
@@ -25,6 +25,24 @@ $('#sort_button').click(function(){
 	$('#sort_display').css('visibility','visible');
 	$('#sort_display').css('opacity','1');
 });
+$('#my_reviews').click(function(){
+	$('#overlay').css('opacity','.4');
+	$('#overlay').css('left','0px');
+	$('#overlay').css('top','0px');
+	$('#overlay').css('visibility','visible');
+	$('#review_display').css('visibility','visible');
+	$('#review_display').css('opacity','1');
+});
+$('#help').click(function(){
+	$('#display').css('left','calc(0px - 50vh)');
+	$('#help_display').css('visibility','visible');
+	$('#help_display').css('opacity','1');
+});
+$('#search').click(function(){
+	$('#display').css('left','calc(0px - 50vh)');
+	$('#search_display').css('visibility','visible');
+	$('#search_display').css('opacity','1');
+});
 $('#overlay').click(function(){
 	$('#overlay').css('opacity','0'); 
 	$('#display').css('left','calc(0px - 50vh)');
@@ -33,4 +51,10 @@ $('#overlay').click(function(){
 	$('profile').css('visibility', 'hidden');
 	$('#sort_display').css('visibility','hidden');
 	$('#sort_display').css('opacity','0');
+	$('#help_display').css('visibility','hidden');
+	$('#help_display').css('opacity','0');
+	$('#search_display').css('visibility','hidden');
+	$('#search_display').css('opacity','0');
+	$('#review_display').css('visibility','hidden');
+	$('#review_display').css('opacity','0');
 });
